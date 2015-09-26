99f3279aa3cda161557861c236f8e2b415ad5ad8
Alex Shawl <schaalander@gmail.com>
2015-09-27 01:37:37 +0200

    initial commit

---
+++ Meal Reviewer Final/www/scripts/index.js
@@ -0,0 +1,52 @@
+﻿// Eine Einführung zur leeren Vorlage finden Sie in der folgenden Dokumentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// So debuggen Sie Code beim Seitenladen in Ripple oder auf Android-Geräten/-Emulatoren: Starten Sie die App, legen Sie Haltepunkte fest, 
+// und führen Sie dann "window.location.reload()" in der JavaScript-Konsole aus.
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+        // TODO: Cordova wurde geladen. Führen Sie hier eine Initialisierung aus, die Cordova erfordert.
+        console.log(navigator.camera);
+
+        document.getElementById("btnTakePhoto").onclick = function () {
+            if (!navigator.camera) {
+                app.showAlert("Camera API not supported", "Error");
+                return;
+            }
+            var options = {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                encodingType: 0     // 0=JPG 1=PNG
+            };
+
+            navigator.camera.getPicture(
+                function (imageUrl) {
+                    var lastPhotoContainer = document.getElementById("mealPhoto");
+                    lastPhotoContainer.innerHTML = "<img src ='" + imageUrl + "' style='width: 75%;' />";
+                },
+                function () {
+                    app.showAlert('Error taking picture', 'Error');
+                },
+                options);
+
+            return false;
+        }
+    };
+
+    function onPause() {
+        // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
+    };
+
+    function onResume() {
+        // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
+    };
+
+
+})();
