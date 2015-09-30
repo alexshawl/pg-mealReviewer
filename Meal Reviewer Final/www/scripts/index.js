18d1c646d2ce48e1ee193bc77382231195d4ba51
Alex Shawl <schaalander@gmail.com>
2015-09-30 17:35:31 +0200

    shaking hinzugefügt

--- Meal Reviewer Final/www/scripts/index.js
+++ Meal Reviewer Final/www/scripts/index.js
@@ -1,6 +1,6 @@
-﻿// TODO: Speicherlogik nutzen wie in http://jsfiddle.net/dmftLt40/
+﻿// TODO: Speicherlogik nutzen wie in http://jsfiddle.net/dmftLt40/ oder besser employee beispiel
 // location fertig machen
-// - Menü bei Klick auf Menü Buttun (Android)
+
 
 var destinationType;
 
@@ -15,17 +15,20 @@ var destinationType;
         document.addEventListener('resume', onResume.bind(this), false);
 
         // Kamera
-        destinationType = navigator.camera.DestinationType;
         if (!navigator.camera) {
             alert("Camera Failure");
+        } else {
+            destinationType = navigator.camera.DestinationType;
         }
+        // Button Foto aufnehmen
         document.getElementById("btnTakePhoto").onclick = function () {
             handleCamera();
-        }
+        };
+        //Button Foto löschen
         document.getElementById("btnDelPhoto").onclick = function () {
             window.localStorage.removeItem("photo1");
             refreshPhoto();
-        }
+        };
 
         //Location
         document.getElementById("btnUpdateLocation").onclick = function () {
@@ -35,24 +38,25 @@ var destinationType;
             window.localStorage.removeItem("locLat1");
             window.localStorage.removeItem("locLong1");
             refreshLocation();
-        }
+        };
+
+        //Google Maps
+        document.getElementById("btnShowMap").onclick = function () {
+            displayRestaurantMap();
+        };
 
     };
 
-    function onPause() {
-        // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
-    };
 
-    function onResume() {
-        // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
-    };
-
+    /**
+    Nimmt ein Foto auf und sppeichert es in der Galerie des Geräts
+    */
     function handleCamera() {
         //alert("Launching Camera");
         navigator.camera.getPicture(onCameraSuccess, onCameraFail, {
             quality: 75,
             destinationType: destinationType.FILE_URI,
-            saveToPhotoAlbum: true,
+            saveToPhotoAlbum: true, // in Galerie Speichern
             correctOrientation: true,
             encodingType: navigator.camera.EncodingType.JPEG
         });
@@ -66,18 +70,19 @@ var destinationType;
         window.localStorage.setItem("photo1", imageData);
 
     };
-
     function onCameraFail(message) {
         alert("Camera failure: " + message);
     };
 
-
+    /**
+    Ruft aktuelle Koordinaten ab und speichert diese
+    */
     function updateLocation() {
         navigator.geolocation.getCurrentPosition(
             function (position) {
                 localStorage.setItem("locLat1", position.coords.latitude);
                 localStorage.setItem("locLong1", position.coords.longitude);
-                refreshLocation();
+                refreshLocation(); // Anzeige der Location neu laden
                 //    document.getElementById("location").innerHTML = position.coords.latitude + " , " + position.coords.longitude;
                 /* alert('Latitude: ' + position.coords.latitude + '\n' +
             'Longitude: ' + position.coords.longitude + '\n' +
@@ -94,14 +99,18 @@ var destinationType;
             });
         return false;
     };
+
+
+    function onPause() {
+        // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
+    };
+
+    function onResume() {
+        // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
+    };
 })();
 
-/**
-Gespeichertes Bild laden
-*/
-$(document).on("pageinit", "#restaurant", function () {
-    refreshPhoto();
-});
+
 function refreshPhoto() {
     var currentPhoto = document.getElementById("restaurantPhoto");
     var savedPhoto = window.localStorage.getItem("photo1");
@@ -114,6 +123,14 @@ function refreshPhoto() {
     }
 };
 
+/**
+Gespeichertes Bild und Location laden
+*/
+$(document).on("pageinit", "#restaurant", function () {
+    refreshPhoto();
+    refreshLocation();
+});
+
 function refreshLocation() {
     var currentLocLat = document.getElementById("locationLat");
     var currentLocLong = document.getElementById("locationLong");
@@ -123,29 +140,102 @@ function refreshLocation() {
     if (savedLocLat != null) {
         currentLocLat.innerHTML = "Latitude=" + savedLocLat;
     } else { // Ansonsten "noimage" Foto anzeigen
-        currentLocLat.innerHTML = "No Location Data";
+        currentLocLat.innerHTML = "No Latitude Data";
     }
     if (savedLocLong != null) {
         currentLocLong.innerHTML = "Longitude=" + savedLocLong;
     } else { // Ansonsten "noimage" Foto anzeigen
-        currentLocLong.innerHTML = "No Location Data";
+        currentLocLong.innerHTML = "No Longitude Data";
     }
 };
 
+function displayRestaurantMap() {
+
+    var lat = window.localStorage.getItem("locLat1");
+    var long = window.localStorage.getItem("locLong1");
+    if (lat != null && long != null) {
+        document.getElementById("map-canvas").hidden = false;
+        document.getElementById("locErrArea").innerHTML = "";
+        drawMap(new google.maps.LatLng(lat, long));
+    } else {
+        document.getElementById("map-canvas").hidden = true;
+        document.getElementById("locErrArea").innerHTML = "No Location Data";
+    }
+};
+
+function drawMap(latlng) {
+    var myOptions = {
+        zoom: 10,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+    // Add an overlay to the map of current lat/lng
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "Greetings!"
+    });
+};
+
+
+$(document).on("pageinit", "#stuffHome", function () {
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+    document.addEventListener("volumedownbutton", onVolumeDownKeyDown, false);
+    document.addEventListener("volumeupbutton", onVolumeUpKeyDown, false);
+    window.addEventListener("batterystatus", onBatteryStatus, false);
+
+    var onShake = function () {
+        var lastPhotoContainer1 = document.getElementById("gruselFoto");
+        lastPhotoContainer1.src = "images/grusel.png";
+        shake.stopWatch();
+    };
+
+    shake.startWatch(onShake, 30);
 
 
 
-/**
-Gespeichertee Location laden
-*/
-$(document).on("pageinit", "#restaurant", function () {
-    refreshLocation();
+    function onBatteryStatus(info) {
+        var status;
+        status = "The Battery Level is <b>" + info.level + "</b>% and the cable is <b>";
+        if (info.isPlugged) {
+            status += "plugged in";
+        } else if (!info.isPlugged) {
+            status += "not plugged in";
+        } else {
+            status += "unknown";
+        }
+        status += "</b>";
+        document.getElementById("pluggedStatus").innerHTML = status;
+    }
+    function onVolumeUpKeyDown() {
+        //  var sliderValue = document.getElementById("slider-2").value;
+        // alert("old value " + document.getElementById("slider-2").value);
+        $("#slider-2").val(1).slider("refresh");
+        //  alert("new value " + document.getElementById("slider-2").value);
+    }
+    function onVolumeDownKeyDown() {
+        var sliderValue = document.getElementById("slider-2").value;
+        $("#slider-2").val(0).slider("refresh");
+    }
+
+    function onMenuKeyDown() {
+        alert("Menu key pressed");
+    }
+
+    function onBackKeyDown() {
+        alert("Back button pressed");
+    }
+
 });
-
 
 /**
 Google Maps
 */
+/**
 $(document).on("pageinit", "#map-page", function () {
     var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
 
@@ -182,5 +272,5 @@ $(document).on("pageinit", "#map-page", function () {
             title: "Greetings!"
         });
     }
-
-});
+    
+});*/
